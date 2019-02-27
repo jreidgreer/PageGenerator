4c6a688f09f85dc78d999311bdc66f5302007460
Reid Greer <jreid.greer@gmail.com>
2019-02-27 12:13:23 -0800

    add boilerplate

--- page-generator.js
+++ page-generator.js
@@ -1,1 +1,13 @@
-console.log('TODO: Make me generator stuff!');
+// Create your mock API response here:
+let DATA;
+
+/**
+ * Renders the DATA into the page. You can pass in whatever you'd like into this function.
+ * There are no specific restrictions around the arguments. jQuery is available to you if
+ * you'd like to use it.
+ */
+function render() {
+  console.log('TODO: Make me generate stuff!');
+}
+
+render();
